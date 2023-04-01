fa64b044d7064133631370844ce47d37de5b6502
George Simei <georgesimei@gmail.com>
2023-04-01 18:09:34 +0200

    exercise 18

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -22,3 +22,20 @@ console.log(population)
 console.log(isIsland)
 console.log(language)
 
+const ageJonas = 2037 - 1991
+
+// Coding Challenge #1
+
+  const markWeight = 78
+  const markHeight = 1.69
+  const johnWeight = 92
+  const johnHeight = 1.95
+
+  const markBMI = markWeight / markHeight ** 2
+  const johnBMI = johnWeight / johnHeight ** 2
+
+  const markHigherBMI = markBMI > johnBMI
+
+  console.log(markBMI, johnBMI, markHigherBMI)
+
+// Strings and Template Literals
