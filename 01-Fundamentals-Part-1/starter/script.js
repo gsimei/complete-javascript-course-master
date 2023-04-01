5f219978cb7f29bda4dbd04d6f596504a8d55403
George Simei <georgesimei@gmail.com>
2023-04-01 02:36:55 +0200

    stoped on class 14 of funtamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+
+let js = 'amazing'
+console.log(40 + 8 + 23 - 10)
+
+'Jonas'
+
+const country = 'Italy'
+const continent = 'Europe'
+let population = 66000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+const isIsland = false
+const language = 'italian'
+
+
+console.log(country)
+console.log(continent)
+console.log(population)
+console.log(isIsland)
+console.log(language)
+
